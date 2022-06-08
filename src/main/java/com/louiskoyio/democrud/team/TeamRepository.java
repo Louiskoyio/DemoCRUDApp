5534cad344612fbe67289a80bd4434408d3913ed
koyio <koyiolouis@gmail.com>
2022-06-08 12:25:05 +0300

    modified application properties

--- src/main/java/com/louiskoyio/democrud/team/TeamRepository.java
+++ src/main/java/com/louiskoyio/democrud/team/TeamRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 public interface TeamRepository extends CrudRepository<Team,Integer> {
     Long countById(Integer id);
 
-    @Query("SELECT Player.fname, Player.lname,Player.nationalId, Player.phoneNumber, Team.teamName FROM Player JOIN Team on Player.teamId = Team.id where Player.teamId =:id")
-    List<Player> players(@Param("id") Integer id);
+    /*@Query("SELECT Player.fname, Player.lname,Player.nationalId, Player.phoneNumber, Team.teamName FROM Player JOIN Team on Player.teamId = Team.id where Player.teamId =:id")
+    List<Player> players(@Param("id") Integer id);*/
 }

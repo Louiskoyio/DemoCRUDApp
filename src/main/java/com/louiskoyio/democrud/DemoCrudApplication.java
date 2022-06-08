1c7a4110725a054713653f7685446e816891ad80
koyio <koyiolouis@gmail.com>
2022-06-08 13:53:23 +0300

    modified application properties

--- src/main/java/com/louiskoyio/democrud/DemoCrudApplication.java
+++ src/main/java/com/louiskoyio/democrud/DemoCrudApplication.java
@@ -2,12 +2,17 @@ package com.louiskoyio.democrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DemoCrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoCrudApplication.class, args);
+    }
+
+    @SpringBootApplication
+    public class SpringBootTomcatApplication extends SpringBootServletInitializer {
     }
 
 }
